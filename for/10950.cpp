4ad4b3e1b70305918bfc83cc9e2ad0a11c6f252c
juheesvt <38813640+juheesvt@users.noreply.github.com>
2019-08-05 16:22:12 +0900

    덧셈

---
+++ for/10950.cpp
@@ -0,0 +1,23 @@
+//
+// Created by juheeSVT on 2019-08-05.
+//
+
+
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b){
+    return a+b;
+}
+
+int main(){
+
+    int t;
+    int a,b;
+
+    cin >> t;
+    for ( int i = 0; i< t; i ++){
+        cin >> a >> b;
+        cout << sum(a,b) << endl;
+    }
+}
