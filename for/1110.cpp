33d1a610aee8d3657e04fc1824ba6ef493363745
juheesvt <38813640+juheesvt@users.noreply.github.com>
2019-08-10 21:33:59 +0900

    더하기사이클 : 무한루프돎

---
+++ for/1110.cpp
@@ -0,0 +1,37 @@
+//
+// Created by juheeSVT on 2019-08-10.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num,aNum,bNum,aPlusB;
+    int result;
+    int cnt=0;
+    cin >> num;
+
+    int initialNum = num;
+
+    while(true){
+        cnt++;
+
+        if (num < 0) {
+            aNum = 0;
+            bNum = num;
+        }
+        else{
+            aNum = num/10;
+            bNum = num%10;
+        }
+        aPlusB = aNum + bNum;
+        result = bNum + aPlusB%10;
+
+        if(result == initialNum){
+            cout << cnt;
+            break;
+        }
+        num = result;
+    }
+}
