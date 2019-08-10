aa7bfab69af728539b2f4e3de91c829b42add191
juheesvt <38813640+juheesvt@users.noreply.github.com>
2019-08-10 16:59:22 +0900

    A+B 4

---
+++ for/10951.cpp
@@ -0,0 +1,21 @@
+//
+// Created by juheeSVT on 2019-08-10.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b;
+
+
+    while(true){
+        cin >> a>> b;
+        if (cin.fail())
+            break;
+
+        cout << a+b <<endl;
+
+    }
+}
